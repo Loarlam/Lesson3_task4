a1a834bab46270e767300519559d0f48a7012517
Loarlam <messiah02@mail.ru>
2020-02-07 15:30:34 +0300

    Add project files.

---
+++ Task4/DocumentWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task4
+{
+    class DocumentWorker
+    {
+        public void OpenDocument()
+        {
+            Console.WriteLine("Документ открыт");
+        }
+
+        public void EditDocument ()
+        {
+            Console.WriteLine("Редактирование документа доступно в версии Про");
+        }
+
+        public void SaveDocument()
+        {
+            Console.WriteLine("Сохранение документа доступно в версии Про");
+        }
+    }
+}
